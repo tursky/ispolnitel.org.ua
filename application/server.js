0f8e1753a6eac223305f80a2f7dedeb4dd9b6996
Stanislav Tursky <stastursky@gmail>
2022-02-13 23:31:30 +0200

    Add some code

--- application/server.js
+++ application/server.js
@@ -1,3 +1,12 @@
 'use strict';
 
+const http = require('http');
+
 // Code
+
+const HOST = 'localhost';
+const PORT = 3000;
+
+http
+  .createServer((req, res) => res.end(`<h1>Node.js server is running...</h1>`))
+  .listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`));
