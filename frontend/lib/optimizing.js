2be447679b2e68b497e02240b310293ac6b82912
S. Tursky <stastursky@gmail>
2022-03-10 14:25:38 +0200

    Improvements and refactoring

--- frontend/lib/optimizing.js
+++ frontend/lib/optimizing.js
@@ -4,124 +4,132 @@
  * https://www.npmjs.com/package/postcss
  */
 
- 'use strict';
+'use strict';
 
- const HTMLTerser = require('html-minifier-terser');
- const Terser = require('terser');
- const postcss = require('postcss');
- const cssnano = require('cssnano');
- const path = require('path');
- const fs = require('fs');
+const HTMLTerser = require('html-minifier-terser');
+const Terser = require('terser');
+const postcss = require('postcss');
+const cssnano = require('cssnano');
+const path = require('path');
+const fs = require('fs');
 
- const config = {
-   ROOT: 'application/static',
-   IGNORE: 'bundles',
- };
+const CONFIGURATIONS = {
+  ROOT: 'application/static',
+  IGNORE: 'bundles',
+  OPTIONS: {
+    JS: { compress: false },
+    HTML: { collapseWhitespace: true, removeComments: true },
+    CSS: [cssnano],
+  },
+};
 
- const options = {
-   JS: { compress: false },
-   CSS: [cssnano],
-   HTML: { collapseWhitespace: true, removeComments: true },
- };
+const SCHEMA = {
+  '.js': (file, options) => handleJS(file, options),
+  '.html': (file, options) => handleHTML(file, options),
+  '.css': (file, options) => handleCSS(file, options),
+};
 
- const schema = {
-   '.css': (file) => handleCSS(file, options.CSS),
-   '.js': (file) => handleJS(file, options.JS),
-   '.html': (file) => handleHTML(file, options.HTML),
- };
+const handleCSS = (file, options) => {
+  const [...plugins] = options;
+  new Promise(() => {
+    fs.readFile(file, (err, css) => {
+      if (err) throw err;
+      postcss(plugins)
+        .process(css, {
+          from: file,
+          to: file,
+        })
+        .then((result) => {
+          fs.writeFile(file, result.css, (e) => {
+            if (e) throw e;
+            printProcessed(file);
+          });
+        });
+    });
+  });
+};
 
- const handleCSS = (file, options) => {
-   const [...plugins] = options;
-   new Promise(() => {
-     fs.readFile(file, (err, css) => {
-       if (err) throw err;
-       postcss(plugins)
-         .process(css, {
-           from: file,
-           to: file,
-         })
-         .then((result) => {
-           fs.writeFile(file, result.css, (e) => {
-             if (e) throw e;
-             printHandlered(file);
-           });
-         });
-     });
-   });
- };
+const handleJS = (file, options) => {
+  new Promise(() => {
+    Terser.minify(fs.readFileSync(file, 'utf8'), options).then((processed) => {
+      fs.writeFile(file, processed.code, (e) => {
+        if (e) throw e;
+        printProcessed(file);
+      });
+    });
+  });
+};
 
- const handleJS = (file, options) => {
-   new Promise(() => {
-     Terser.minify(fs.readFileSync(file, 'utf8'), options).then((processed) => {
-       fs.writeFile(file, processed.code, (e) => {
-         if (e) throw e;
-         printHandlered(file);
-       });
-     });
-   });
- };
+const handleHTML = (file, options) => {
+  new Promise(() => {
+    HTMLTerser.minify(fs.readFileSync(file, 'utf8'), options).then(
+      (processed) => {
+        fs.writeFile(file, processed, (e) => {
+          if (e) throw e;
+          printProcessed(file);
+        });
+      }
+    );
+  });
+};
 
- const handleHTML = (file, options) => {
-   new Promise(() => {
-     HTMLTerser.minify(fs.readFileSync(file, 'utf8'), options).then(
-       (processed) => {
-         fs.writeFile(file, processed, (e) => {
-           if (e) throw e;
-           printHandlered(file);
-         });
-       }
-     );
-   });
- };
+const printProcessed = (file) => console.log(`✅ ${file}`);
 
- const check = (directory) => {
-   const result = fs.existsSync(directory);
-   if (result === false) console.table([{ ERROR: `${directory} not found!` }]);
-   return result;
- };
+const pathfinder = (directory, exception, metadata) => {
+  const isExist = check(directory);
+  if (!isExist) return 0;
 
- const pathfinder = (dir, exception) => {
-   const isExist = check(dir);
-   if (!isExist) return 0;
+  const files = fs.readdirSync(directory);
 
-   const files = fs.readdirSync(dir);
+  for (let i = 0; i < files.length; i++) {
+    const pathname = path.join(directory, files[i]);
+    const status = fs.lstatSync(pathname);
+    if (status.isDirectory()) pathfinder(pathname, exception, metadata);
+    if (pathname.includes(exception)) continue;
+    processing(pathname, metadata);
+  }
+};
 
-   for (let i = 0; i < files.length; i++) {
-     const pathname = path.join(dir, files[i]);
-     const status = fs.lstatSync(pathname);
-     if (status.isDirectory()) pathfinder(pathname, exception);
-     if (pathname.includes(exception)) continue;
-     handler(pathname);
-   }
- };
+const check = (path) => {
+  const result = fs.existsSync(path);
+  const err = `❗️ Path "${path}" not found! \n`;
+  if (result === false) console.log(err);
+  return result;
+};
 
- const handler = (file) => {
-   const ext = path.extname(file);
-   const scenario = schema[ext];
-   processing(file, scenario);
- };
+const processing = (filepath, metadata) => {
+  const ext = path.extname(filepath);
+  const scenario = SCHEMA[ext];
+  const format = ext.slice(1).toUpperCase();
+  const options = metadata[format];
+  handler(filepath, scenario, options);
+};
 
- const processing = (filepath, handler) => {
-   const type = typeof handler;
-   if (type === 'function') {
-     const serializer = types[type];
-     serializer([handler, filepath], (handler) => processing(filepath, handler));
-   }
- };
+const handler = (file, scenario, options) => {
+  const type = typeof scenario;
+  if (type === 'function') {
+    const serializer = types[type];
+    serializer([scenario, file, options], (scenario) =>
+      handler(file, scenario, options)
+    );
+  }
+};
 
- const types = {
-   object: ([todo], callback) => callback(JSON.stringify(todo)),
-   undefined: (callback) => callback('not found'),
-   function: ([fn, filepath], callback) =>
-     callback(JSON.stringify(fn(filepath))),
- };
+const types = {
+  object: ([obj], callback) => callback(JSON.stringify(obj)),
+  undefined: (callback) => callback('not found'),
+  function: ([fn, filepath, options], callback) =>
+    callback(JSON.stringify(fn(filepath, options))),
+};
 
- const printHandlered = (file) => console.log(`✅ ${file}`);
+const renderTitle = () => {
+  process.stdout.write('\x1Bc');
+  console.log('\x1b[1;33m');
+  console.log('FRONTEND OPTIMIZING...');
+  console.log('\x1b[0m');
+};
 
- const ROOT = config.ROOT;
- const IGNORE = config.IGNORE;
+const { ROOT, IGNORE, OPTIONS } = CONFIGURATIONS;
 
- process.stdout.write('\x1Bc');
- console.log('\x1b[1;33m' + 'FRONTEND OPTIMIZING...' + '\x1b[0m' + '\n');
-
- pathfinder(ROOT, IGNORE);
+renderTitle();
+pathfinder(ROOT, IGNORE, OPTIONS);
