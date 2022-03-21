1516a03fd55064ac0b4af43e748ca83dee58c2a5
S. Tursky <tursky.sv@protonmail.com>
2022-03-21 16:09:54 +0200

    Delete old schema of cli properties

--- frontend/lib/optimizer.js
+++ frontend/lib/optimizer.js
@@ -80,49 +80,6 @@ const getConsoleRenderSettings = () => ({
 		newline: (n, str = '\n') => str.repeat(n),
 		space: (n, str = ' ') => str.repeat(n),
 	},
-
-  /**
-   * CONSOLE TYPOGRAPHY
-   */
-  clear: '\x1Bc',
-  reset: '\x1b[0m',
-  boldfont: '\x1b[1m',
-  hidden: '\x1b[8m',
-  underline: '\x1b[4m',
-  dim: '\x1b[2m',
-  blink: '\x1b[5m',
-  reverse: '\x1b[7m',
-
-  /**
-   * CONSOLE COLORS
-   */
-  black: '\x1b[30m',
-  red: '\x1b[31m',
-  green: '\x1b[32m',
-  yellow: '\x1b[33m',
-  blue: '\x1b[34m',
-  magenta: '\x1b[35m',
-  cyan: '\x1b[36m',
-  white: '\x1b[37m',
-
-  /**
-   * CONSOLE BACKGROUNDS
-   */
-  blackBG: '\x1b[40m',
-  redBG: '\x1b[41m',
-  greenBG: '\x1b[42m',
-  yellowBG: '\x1b[43m',
-  blueBG: '\x1b[44m',
-  magentaBG: '\x1b[45m',
-  cyanBG: '\x1b[46m',
-  whiteBG: '\x1b[47m',
-
-  /**
-   * CONSOLE FUNCTIONALITY
-   */
-  draw: (symbol) => symbol,
-  newline: (n = 1, str = '\n') => str.repeat(n),
-  space: (n = 1, str = ' ') => str.repeat(n),
 });
 
 const start = (app, stdout = getConsoleRenderSettings()) => {
