fe938513af60bc97744a51e8bb49dbdfa0b474fc
S. Tursky <tursky.sv@protonmail.com>
2022-03-21 16:22:25 +0200

    Formatting

--- frontend/lib/optimizer.js
+++ frontend/lib/optimizer.js
@@ -38,48 +38,47 @@ const render = (string) => process.stdout.write(string);
 const preprint = (array) => array.join('');
 
 const getConsoleRenderPreferences = () => ({
-
   display: {
-		clear: '\x1Bc',
-		reset: '\x1b[0m',
-	},
+    clear: '\x1Bc',
+    reset: '\x1b[0m',
+  },
 
-	color: {
-		black: '\x1b[30m',
-		red: '\x1b[31m',
-		green: '\x1b[32m',
-		yellow: '\x1b[33m',
-		blue: '\x1b[34m',
-		magenta: '\x1b[35m',
-		cyan: '\x1b[36m',
-		white: '\x1b[37m',
-	},
+  color: {
+    black: '\x1b[30m',
+    red: '\x1b[31m',
+    green: '\x1b[32m',
+    yellow: '\x1b[33m',
+    blue: '\x1b[34m',
+    magenta: '\x1b[35m',
+    cyan: '\x1b[36m',
+    white: '\x1b[37m',
+  },
 
-	background: {
-		black: '\x1b[40m',
-		red: '\x1b[41m',
-		green: '\x1b[42m',
-		yellow: '\x1b[43m',
-		blue: '\x1b[44m',
-		magenta: '\x1b[45m',
-		cyan: '\x1b[46m',
-		white: '\x1b[47m',
-	},
+  background: {
+    black: '\x1b[40m',
+    red: '\x1b[41m',
+    green: '\x1b[42m',
+    yellow: '\x1b[43m',
+    blue: '\x1b[44m',
+    magenta: '\x1b[45m',
+    cyan: '\x1b[46m',
+    white: '\x1b[47m',
+  },
 
-	text: {
-		boldfont: '\x1b[1m',
-		hidden: '\x1b[8m',
-		underline: '\x1b[4m',
-		dim: '\x1b[2m',
-		blink: '\x1b[5m',
-		reverse: '\x1b[7m',
-	},
+  text: {
+    boldfont: '\x1b[1m',
+    hidden: '\x1b[8m',
+    underline: '\x1b[4m',
+    dim: '\x1b[2m',
+    blink: '\x1b[5m',
+    reverse: '\x1b[7m',
+  },
 
-	fn: {
-		draw: (symbol) => symbol,
-		newline: (n, str = '\n') => str.repeat(n),
-		space: (n, str = ' ') => str.repeat(n),
-	},
+  fn: {
+    draw: (symbol) => symbol,
+    newline: (n, str = '\n') => str.repeat(n),
+    space: (n, str = ' ') => str.repeat(n),
+  },
 });
 
 const start = (app, cli = getConsoleRenderPreferences()) => {
