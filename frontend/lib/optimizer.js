2ed387627d258b581f53c202c00d3f0a7d5239a0
S. Tursky <tursky.sv@protonmail.com>
2022-03-22 14:38:53 +0200

    Fix formatting

--- frontend/lib/optimizer.js
+++ frontend/lib/optimizer.js
@@ -4,301 +4,301 @@
  * https://www.npmjs.com/package/postcss
  */
 
- 'use strict';
+'use strict';
 
- const HTMLTerser = require('html-minifier-terser');
- const Terser = require('terser');
- const postcss = require('postcss');
- const cssnano = require('cssnano');
- const path = require('path');
- const fs = require('fs');
+const HTMLTerser = require('html-minifier-terser');
+const Terser = require('terser');
+const postcss = require('postcss');
+const cssnano = require('cssnano');
+const path = require('path');
+const fs = require('fs');
 
- const CONFIGURATIONS = {
-   ROOT: 'application/static',
-   OPTIONS: {
-     JS: { compress: false },
-     HTML: { collapseWhitespace: true, removeComments: true },
-     CSS: [cssnano],
-   },
-   IGNORE: [
-     'bundles',
-     'images',
-     'webfonts',
-     'docs',
-     'robots.txt',
-     'sitemap.xml',
-     'mail.php',
-     'libs.zip',
-   ],
- };
+const CONFIGURATIONS = {
+  ROOT: 'application/static',
+  OPTIONS: {
+    JS: { compress: false },
+    HTML: { collapseWhitespace: true, removeComments: true },
+    CSS: [cssnano],
+  },
+  IGNORE: [
+    'bundles',
+    'images',
+    'webfonts',
+    'docs',
+    'robots.txt',
+    'sitemap.xml',
+    'mail.php',
+    'libs.zip',
+  ],
+};
 
- const application = 'FRONTEND OPTIMIZER';
+const application = 'FRONTEND OPTIMIZER';
 
- const render = (output) => process.stdout.write(output);
- const preprint = (array) => array.join('');
+const render = (output) => process.stdout.write(output);
+const preprint = (array) => array.join('');
 
- const getConsoleRenderPreferences = () => ({
-   display: {
-     clear: '\x1Bc',
-     reset: '\x1b[0m',
-   },
+const getConsoleRenderPreferences = () => ({
+  display: {
+    clear: '\x1Bc',
+    reset: '\x1b[0m',
+  },
 
-   color: {
-     black: '\x1b[30m',
-     red: '\x1b[31m',
-     green: '\x1b[32m',
-     yellow: '\x1b[33m',
-     blue: '\x1b[34m',
-     magenta: '\x1b[35m',
-     cyan: '\x1b[36m',
-     white: '\x1b[37m',
-   },
+  color: {
+    black: '\x1b[30m',
+    red: '\x1b[31m',
+    green: '\x1b[32m',
+    yellow: '\x1b[33m',
+    blue: '\x1b[34m',
+    magenta: '\x1b[35m',
+    cyan: '\x1b[36m',
+    white: '\x1b[37m',
+  },
 
-   background: {
-     black: '\x1b[40m',
-     red: '\x1b[41m',
-     green: '\x1b[42m',
-     yellow: '\x1b[43m',
-     blue: '\x1b[44m',
-     magenta: '\x1b[45m',
-     cyan: '\x1b[46m',
-     white: '\x1b[47m',
-   },
+  background: {
+    black: '\x1b[40m',
+    red: '\x1b[41m',
+    green: '\x1b[42m',
+    yellow: '\x1b[43m',
+    blue: '\x1b[44m',
+    magenta: '\x1b[45m',
+    cyan: '\x1b[46m',
+    white: '\x1b[47m',
+  },
 
-   text: {
-     boldfont: '\x1b[1m',
-     hidden: '\x1b[8m',
-     underline: '\x1b[4m',
-     dim: '\x1b[2m',
-     blink: '\x1b[5m',
-     reverse: '\x1b[7m',
-   },
+  text: {
+    boldfont: '\x1b[1m',
+    hidden: '\x1b[8m',
+    underline: '\x1b[4m',
+    dim: '\x1b[2m',
+    blink: '\x1b[5m',
+    reverse: '\x1b[7m',
+  },
 
-   fn: {
-     draw: (string) => string,
-     newline: (n) => '\n'.repeat(n),
-     space: (n) => ' '.repeat(n),
-   },
- });
+  fn: {
+    draw: (string) => string,
+    newline: (n) => '\n'.repeat(n),
+    space: (n) => ' '.repeat(n),
+  },
+});
 
- const start = (app, cli = getConsoleRenderPreferences()) => {
-   render(
-     preprint([
-       cli.display.clear,
-       cli.text.boldfont,
-       cli.color.white,
-       cli.background.blue,
-       cli.fn.draw(cli.fn.space(5) + cli.fn.space(app.length) + cli.fn.space(5)),
-       cli.fn.newline(1),
-       cli.fn.space(5),
-       app,
-       cli.fn.space(5),
-       cli.fn.newline(1),
-       cli.fn.draw(cli.fn.space(5) + cli.fn.space(app.length) + cli.fn.space(5)),
-       cli.fn.newline(3),
-       cli.display.reset,
-     ])
-   );
- };
+const start = (app, cli = getConsoleRenderPreferences()) => {
+  render(
+    preprint([
+      cli.display.clear,
+      cli.text.boldfont,
+      cli.color.white,
+      cli.background.blue,
+      cli.fn.draw(cli.fn.space(5) + cli.fn.space(app.length) + cli.fn.space(5)),
+      cli.fn.newline(1),
+      cli.fn.space(5),
+      app,
+      cli.fn.space(5),
+      cli.fn.newline(1),
+      cli.fn.draw(cli.fn.space(5) + cli.fn.space(app.length) + cli.fn.space(5)),
+      cli.fn.newline(3),
+      cli.display.reset,
+    ])
+  );
+};
 
- const output = (
-   file,
-   handled = '[ok]',
-   cli = getConsoleRenderPreferences()
- ) => {
-   render(
-     preprint([
-       cli.color.cyan,
-       handled,
-       cli.color.blue,
-       cli.text.dim,
-       cli.fn.draw(' - '),
-       cli.display.reset,
-       cli.color.blue,
-       file,
-       cli.fn.newline(1),
-       cli.display.reset,
-     ])
-   );
- };
+const output = (
+  file,
+  handled = '[ok]',
+  cli = getConsoleRenderPreferences()
+) => {
+  render(
+    preprint([
+      cli.color.cyan,
+      handled,
+      cli.color.blue,
+      cli.text.dim,
+      cli.fn.draw(' - '),
+      cli.display.reset,
+      cli.color.blue,
+      file,
+      cli.fn.newline(1),
+      cli.display.reset,
+    ])
+  );
+};
 
- const handleError = (
-   file,
-   err,
-   unhandled = '[ok]',
-   cli = getConsoleRenderPreferences()
- ) => {
-   render(
-     preprint([
-       cli.color.red,
-       unhandled,
-       cli.color.blue,
-       cli.text.dim,
-       cli.fn.draw(' - '),
-       cli.display.reset,
-       cli.color.blue,
-       file,
-       cli.fn.newline(2),
-       cli.color.red,
-       err.stack,
-       cli.fn.newline(2),
-       cli.display.reset,
-     ])
-   );
-   process.exit();
- };
+const handleError = (
+  file,
+  err,
+  unhandled = '[ok]',
+  cli = getConsoleRenderPreferences()
+) => {
+  render(
+    preprint([
+      cli.color.red,
+      unhandled,
+      cli.color.blue,
+      cli.text.dim,
+      cli.fn.draw(' - '),
+      cli.display.reset,
+      cli.color.blue,
+      file,
+      cli.fn.newline(2),
+      cli.color.red,
+      err.stack,
+      cli.fn.newline(2),
+      cli.display.reset,
+    ])
+  );
+  process.exit();
+};
 
- const handleCSS = async (file, config) => {
-   try {
-     const content = fs.readFileSync(file, 'utf8');
-     const plugins = [...config];
-     const processed = await postcss(plugins).process(content, {
-       from: file,
-       to: file,
-     });
-     fs.writeFileSync(file, processed.css);
-   } catch (err) {
-     handleError(file, err);
-   }
-   output(file);
- };
+const handleCSS = async (file, config) => {
+  try {
+    const content = fs.readFileSync(file, 'utf8');
+    const plugins = [...config];
+    const processed = await postcss(plugins).process(content, {
+      from: file,
+      to: file,
+    });
+    fs.writeFileSync(file, processed.css);
+  } catch (err) {
+    handleError(file, err);
+  }
+  output(file);
+};
 
- const handleJS = async (file, config) => {
-   try {
-     const content = fs.readFileSync(file, 'utf8');
-     const options = { ...config };
-     const processed = await Terser.minify(content, options);
-     fs.writeFileSync(file, processed.code);
-   } catch (err) {
-     handleError(file, err);
-   }
-   output(file);
- };
+const handleJS = async (file, config) => {
+  try {
+    const content = fs.readFileSync(file, 'utf8');
+    const options = { ...config };
+    const processed = await Terser.minify(content, options);
+    fs.writeFileSync(file, processed.code);
+  } catch (err) {
+    handleError(file, err);
+  }
+  output(file);
+};
 
- const handleHTML = async (file, config) => {
-   try {
-     const content = fs.readFileSync(file, 'utf8');
-     const options = { ...config };
-     const processed = await HTMLTerser.minify(content, options);
-     fs.writeFileSync(file, processed);
-   } catch (err) {
-     handleError(file, err);
-   }
-   output(file);
- };
+const handleHTML = async (file, config) => {
+  try {
+    const content = fs.readFileSync(file, 'utf8');
+    const options = { ...config };
+    const processed = await HTMLTerser.minify(content, options);
+    fs.writeFileSync(file, processed);
+  } catch (err) {
+    handleError(file, err);
+  }
+  output(file);
+};
 
- const schema = {
-   '.js': (file, options) => handleJS(file, options),
-   '.html': (file, options) => handleHTML(file, options),
-   '.css': (file, options) => handleCSS(file, options),
- };
+const schema = {
+  '.js': (file, options) => handleJS(file, options),
+  '.html': (file, options) => handleHTML(file, options),
+  '.css': (file, options) => handleCSS(file, options),
+};
 
- const types = {
-   object: ([obj], callback) => callback(JSON.stringify(obj)),
-   undefined: (callback) => callback('not found'),
-   function: ([fn, filepath, options], callback) =>
-     callback(JSON.stringify(fn(filepath, options))),
- };
+const types = {
+  object: ([obj], callback) => callback(JSON.stringify(obj)),
+  undefined: (callback) => callback('not found'),
+  function: ([fn, filepath, options], callback) =>
+    callback(JSON.stringify(fn(filepath, options))),
+};
 
- const handler = (file, intention, options) => {
-   const type = typeof intention;
-   if (type === 'function') {
-     const serializer = types[type];
-     serializer([intention, file, options], (intention) =>
-       handler(file, intention, options)
-     );
-   }
- };
+const handler = (file, intention, options) => {
+  const type = typeof intention;
+  if (type === 'function') {
+    const serializer = types[type];
+    serializer([intention, file, options], (intention) =>
+      handler(file, intention, options)
+    );
+  }
+};
 
- const preprocess = (filepath, metadata) => {
-   const ext = path.extname(filepath);
-   const scenario = schema[ext];
-   const format = ext.slice(1).toUpperCase();
-   const instruction = metadata[format];
-   handler(filepath, scenario, instruction);
- };
+const preprocess = (filepath, metadata) => {
+  const ext = path.extname(filepath);
+  const scenario = schema[ext];
+  const format = ext.slice(1).toUpperCase();
+  const instruction = metadata[format];
+  handler(filepath, scenario, instruction);
+};
 
- const getDirectoryContent = (directory) => fs.readdirSync(directory);
- const getSourceDetails = (source) => fs.lstatSync(source);
+const getDirectoryContent = (directory) => fs.readdirSync(directory);
+const getSourceDetails = (source) => fs.lstatSync(source);
 
- const verifySourceExclusion = (path, filter) =>
-   filter.find((exclusion) => path.includes(exclusion));
+const verifySourceExclusion = (path, filter) =>
+  filter.find((exclusion) => path.includes(exclusion));
 
- const pathfinder = (root, exclusions, metadata) => {
-   const src = getDirectoryContent(root);
-   for (let source of src) {
-     const pathname = path.join(root, source);
-     source = getSourceDetails(pathname);
-     if (source.isDirectory()) {
-       pathfinder(pathname, exclusions, metadata);
-       continue;
-     }
-     if (verifySourceExclusion(pathname, exclusions)) continue;
-     preprocess(pathname, metadata);
-   }
- };
+const pathfinder = (root, exclusions, metadata) => {
+  const src = getDirectoryContent(root);
+  for (let source of src) {
+    const pathname = path.join(root, source);
+    source = getSourceDetails(pathname);
+    if (source.isDirectory()) {
+      pathfinder(pathname, exclusions, metadata);
+      continue;
+    }
+    if (verifySourceExclusion(pathname, exclusions)) continue;
+    preprocess(pathname, metadata);
+  }
+};
 
- const EXIT = {
-   SUCCESS: true,
-   FAILURE: false,
- };
+const EXIT = {
+  SUCCESS: true,
+  FAILURE: false,
+};
 
- const saveExitInformation = (
-   data = 'Exit info is missing...',
-   obj = EXIT,
-   field = 'INFO'
- ) => {
-   Object.defineProperty(obj, field, { value: data });
- };
+const saveExitInformation = (
+  data = 'Exit info is missing...',
+  obj = EXIT,
+  field = 'INFO'
+) => {
+  Object.defineProperty(obj, field, { value: data });
+};
 
- const getExitInformation = (
-   obj = EXIT,
-   field = 'INFO',
-   response = obj.hasOwnProperty.call(obj, field)
-     ? obj[field]
-     : 'Data is missing...'
- ) => response;
+const getExitInformation = (
+  obj = EXIT,
+  field = 'INFO',
+  response = obj.hasOwnProperty.call(obj, field)
+    ? obj[field]
+    : 'Data is missing...'
+) => response;
 
- const verifyDirExists = (path) => fs.existsSync(path);
+const verifyDirExists = (path) => fs.existsSync(path);
 
- const main = (settings) => {
-   const { ROOT, IGNORE, OPTIONS } = settings;
-   start(application);
-   if (verifyDirExists(ROOT) === false) {
-     const data = `Target directory not found! Path incorrect: ${ROOT}`;
-     saveExitInformation(data);
-     return EXIT.FAILURE;
-   }
-   pathfinder(ROOT, IGNORE, OPTIONS);
-   return EXIT.SUCCESS;
- };
+const main = (settings) => {
+  const { ROOT, IGNORE, OPTIONS } = settings;
+  start(application);
+  if (verifyDirExists(ROOT) === false) {
+    const data = `Target directory not found! Path incorrect: ${ROOT}`;
+    saveExitInformation(data);
+    return EXIT.FAILURE;
+  }
+  pathfinder(ROOT, IGNORE, OPTIONS);
+  return EXIT.SUCCESS;
+};
 
- const reportFailure = (
-   info,
-   warning = 'Failure',
-   cli = getConsoleRenderPreferences()
- ) => {
-   render(
-     preprint([
-       cli.text.boldfont,
-       cli.color.blue,
-       cli.fn.draw('- '),
-       warning,
-       cli.fn.draw('❗️'),
-       cli.fn.newline(1),
-       cli.fn.draw('- '),
-       info,
-       cli.fn.newline(3),
-       cli.display.reset,
-     ])
-   );
- };
+const reportFailure = (
+  info,
+  warning = 'Failure',
+  cli = getConsoleRenderPreferences()
+) => {
+  render(
+    preprint([
+      cli.text.boldfont,
+      cli.color.blue,
+      cli.fn.draw('- '),
+      warning,
+      cli.fn.draw('❗️'),
+      cli.fn.newline(1),
+      cli.fn.draw('- '),
+      info,
+      cli.fn.newline(3),
+      cli.display.reset,
+    ])
+  );
+};
 
- // Start process
- const fn = main(CONFIGURATIONS);
+// Start process
+const fn = main(CONFIGURATIONS);
 
- if (fn === EXIT.FAILURE) {
-   const info = getExitInformation();
-   reportFailure(info);
-   process.exit();
- }
+if (fn === EXIT.FAILURE) {
+  const info = getExitInformation();
+  reportFailure(info);
+  process.exit();
+}
