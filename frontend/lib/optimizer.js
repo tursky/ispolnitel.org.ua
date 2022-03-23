a078454e5fddff9ef7336bf38ece085c9c02c3cf
S. Tursky <tursky.sv@protonmail.com>
2022-03-23 22:24:03 +0200

    Replace old code

--- frontend/lib/optimizer.js
+++ frontend/lib/optimizer.js
@@ -38,49 +38,7 @@ const application = 'FRONTEND OPTIMIZER';
 const render = (output) => process.stdout.write(output);
 const preprint = (arr) => arr.join('');
 
-const getConsoleTypography = () => ({
-  text: {
-    boldfont: '\x1b[1m',
-    hidden: '\x1b[8m',
-    underline: '\x1b[4m',
-    dim: '\x1b[2m',
-    blink: '\x1b[5m',
-    reverse: '\x1b[7m',
-  },
-
-  color: {
-    black: '\x1b[30m',
-    red: '\x1b[31m',
-    green: '\x1b[32m',
-    yellow: '\x1b[33m',
-    blue: '\x1b[34m',
-    magenta: '\x1b[35m',
-    cyan: '\x1b[36m',
-    white: '\x1b[37m',
-  },
-
-  background: {
-    black: '\x1b[40m',
-    red: '\x1b[41m',
-    green: '\x1b[42m',
-    yellow: '\x1b[43m',
-    blue: '\x1b[44m',
-    magenta: '\x1b[45m',
-    cyan: '\x1b[46m',
-    white: '\x1b[47m',
-  },
-
-  display: {
-    clear: '\x1Bc',
-    reset: '\x1b[0m',
-  },
-
-  fn: {
-    draw: (struct) => struct,
-    newline: (n) => '\n'.repeat(n),
-    space: (n) => ' '.repeat(n),
-  },
-});
+const getConsoleTypography = () => ConsoleUI;
 
 const start = (app, cli = getConsoleTypography()) => {
   render(
